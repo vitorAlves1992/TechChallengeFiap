d5939fe692cffd8f80f556801df3b8ac98489de0
vitorAlves1992 <vitoralves1992@gmail.com>
2023-05-23 21:17:39 -0300

    Ajustado para salvar

--- Tech-Challenge/src/main/java/com/fiap/techChallenge/TechChallenge/domain/Eletrodomestico.java
+++ Tech-Challenge/src/main/java/com/fiap/techChallenge/TechChallenge/domain/Eletrodomestico.java
@@ -1,9 +1,12 @@
 package com.fiap.techChallenge.TechChallenge.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 @Component
+@Getter@Setter
 public class Eletrodomestico {
     private Integer idUsuario;
     private Integer id ;
--- Tech-Challenge/src/main/java/com/fiap/techChallenge/TechChallenge/repository/EletrodomesticoRepository.java
+++ Tech-Challenge/src/main/java/com/fiap/techChallenge/TechChallenge/repository/EletrodomesticoRepository.java
@@ -3,11 +3,12 @@ package com.fiap.techChallenge.TechChallenge.repository;
 import com.fiap.techChallenge.TechChallenge.domain.Eletrodomestico;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Random;
 @Repository
 public class EletrodomesticoRepository {
-    private Collection <Eletrodomestico> eletrodomesticos;
+    private Collection <Eletrodomestico> eletrodomesticos = new ArrayList<>();
 
     public Eletrodomestico salvar (Eletrodomestico eletrodomestico){
         eletrodomestico.setId();
