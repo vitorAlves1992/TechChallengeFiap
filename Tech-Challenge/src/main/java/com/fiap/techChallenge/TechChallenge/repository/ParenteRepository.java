e76d1f6b79cc56cc8f2f2c6d6783518601ff7e27
Felipe Santos <felipe.dss17@gmail.com>
2023-08-25 17:28:23 -0300

    salvando automaticamente relacionamento reverso entre Usuário e Pessoa

--- Tech-Challenge/src/main/java/com/fiap/techChallenge/TechChallenge/repository/ParenteRepository.java
+++ Tech-Challenge/src/main/java/com/fiap/techChallenge/TechChallenge/repository/ParenteRepository.java
@@ -2,6 +2,7 @@ package com.fiap.techChallenge.TechChallenge.repository;
 
 import com.fiap.techChallenge.TechChallenge.domain.Parente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface ParenteRepository extends JpaRepository<Parente, Long> {
     List<Parente> findByPessoaId(Long id);
+
+    @Query(value = "select p from Parente p where p.pessoa.id = :pessoaId and p.pessoaRelacionada.id = :usuarioId")
+    Parente findRelacionamentoBase(Long pessoaId, Long usuarioId);
 }
